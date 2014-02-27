59388254f22c0ca0075fe1fe62e3e54b4360fae4
Nazgugu <prada520farrell@gmail.com>
2014-02-27 14:16:55 -0800

    added some other methods

--- src/edu/ucsb/cs56/projects/utilities/calculator/Calculator.java
+++ src/edu/ucsb/cs56/projects/utilities/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs56.projects.utilities.calculator;
 
 import java.lang.NumberFormatException;
+import java.lang.Math;
 
 
 /**
@@ -9,9 +10,10 @@ import java.lang.NumberFormatException;
  */
 class Calculator {
 
-	private String left, operator, right;
-	private boolean onRightSide; // true if appending to right side of expression, false if appending to left side
+	private String left, operator, right, tempL, tempOp, tempR;
+	private boolean onRightSide, isInParenthesis, hasParenthesis; // true if appending to right side of expression, false if appending to left side
 	private JLabelMessageDestination display;
+    private int locationOfParen; //where is the parenthesis, on the left = 0, on the right = 1;
     /**
        Constructor
        @param display The JLabelMessageDestination to send the operations and results to
@@ -21,6 +23,9 @@ class Calculator {
 		operator = "";
 		right = "";
 		onRightSide = false;
+        isInParenthesis = false;
+        hasParenthesis = false;
+        locationOfParen = 0;
 		this.display = display;
 		refresh();
 	}
@@ -28,20 +33,88 @@ class Calculator {
        Call this method with a String to have the calculator do some operation (i.e. appending a digit to the current number, or appending an operator to the expression)
      */
 	public void append(String s){
-		if(s.equals("Enter")){
+		System.out.println("S = " + s);
+        System.out.println(isInParenthesis);
+        if(s.equals("Enter")){
 			operate();
 			return;
 		}
+        if (isInParenthesis)
+        {
+            if (s.equals(")"))
+            {
+                isInParenthesis = false;
+                //System.out.println("bool here is: " + isInParenthesis);
+                if (onRightSide)
+                {
+                    right = right + s;
+                    //onRightSide = false;
+                }
+                else
+                {
+                    left = left + s;
+                    onRightSide = true;
+                }
+            }
+            else if (s.equals("Clear"))
+                clear();
+            else if (s.equals("Delete"))
+                delete();
+            else if (onRightSide)
+                right = right + s;
+            else
+                left = left + s;
+        }
+        else
+        {
 		if(s.equals("Clear"))
 			clear();
 		else if(s.equals("Delete"))
 			delete();
-		else if(s.equals("-") && !onRightSide && left.equals(""))
+        else if (s.equals("("))
+        {
+            isInParenthesis = true;
+            hasParenthesis = true;
+            if (onRightSide)
+            {
+                right = right + s;
+                locationOfParen = 1;
+            }
+            else
+                left = left + s;
+        }
+        else if (s.equals("cos") && !onRightSide && left.equals(""))
+        {
+            left = left + "";
+            if(operator.equals("") || right.equals("")){
+				operator = s;
+				onRightSide = true;
+            }
+        }
+
+        else if (s.equals("sin") && !onRightSide && left.equals(""))
+        {
+            left = left + "";
+            if(operator.equals("") || right.equals("")){
+				operator = s;
+				onRightSide = true;
+            }
+        }
+        
+        else if (s.equals("sqrt") && !onRightSide && left.equals(""))
+        {
+            left = left + "";
+            if(operator.equals("") || right.equals("")){
+				operator = s;
+				onRightSide = true;
+            }
+        }
+        else if(s.equals("-") && !onRightSide && left.equals(""))
 			left = left + s;
 		else if(s.equals("-") && onRightSide && !operator.equals("") && right.equals(""))
-			right = right + s; 
-		else if(s.equals("*") || s.equals("+") || s.equals("-") || s.equals("/")){
-			if(operator.equals("") || right.equals("")){
+			right = right + s;
+		else if(s.equals("*") || s.equals("+") || s.equals("-") || s.equals("/") || s.equals("^")){
+            if(operator.equals("") || right.equals("")){
 				operator = s;
 				onRightSide = true;
 			}
@@ -50,9 +123,11 @@ class Calculator {
 			right = right + s;
 		else
 			left = left + s;
+        }
 		refresh();
-
-	}
+    }
+	
+        
     /**
        Refresh the display to update it to the current state of the expression
      */
@@ -67,10 +142,16 @@ class Calculator {
        Clear out the expression and refresh the display
      */
 	public void clear(){
-		left = "";
+		System.out.println("left is: " + left);
+        System.out.println("right is: " + right);
+        System.out.println("operator is: " + operator);
+        left = "";
 		operator = "";
 		right = "";
 		onRightSide = false;
+        isInParenthesis = false;
+        hasParenthesis = false;
+        locationOfParen = 0;
 		refresh();
 	}
     /**
@@ -78,15 +159,39 @@ class Calculator {
      */
 	public void delete(){
 		if(onRightSide){
+            //System.out.println("i am on the right");
 			if(right.equals("")){
 				operator = "";
 				onRightSide = false;
 			}
 			else
-				right = right.substring(0, right.length() - 1);
+            {
+                char end = right.charAt(right.length() - 1);
+                System.out.println("end is " + end);
+                if (end == '(')
+                {
+                    //System.out.println("begin parenthesis");
+                    isInParenthesis = false;
+                }
+                else if (end == ')')
+                    isInParenthesis = true;
+                right = right.substring(0, right.length() - 1);
+            }
 		}else{
+            //System.out.println("I am not on the right");
 			if(!left.equals(""))
+            {
+                char end  = left.charAt(left.length() - 1);
+                System.out.println("end is " + end);
+                if (end == '(')
+                {
+                    //System.out.println("begin parenthesis second case");
+                    isInParenthesis = false;
+                }
+                else if (end == ')')
+                    isInParenthesis = true;
 				left = left.substring(0,left.length() - 1);
+            }
 		}
 	}
     /**
@@ -95,7 +200,60 @@ class Calculator {
 	public void operate(){
 		double result = 0.0;
 		try{
-		if(left.equals("") || operator.equals("") || right.equals("")){ return; }
+            if (hasParenthesis)
+            {
+                if (locationOfParen == 0) //parenthesis on the left
+                {
+                    
+                }
+                else  //parenthesis on the left
+                {
+                    
+                }
+            }
+            else
+            {
+                if(left.equals(""))
+                    {
+            if (operator.equals("") || right.equals(""))
+                return;
+            else
+            {
+                if (operator.equals("sqrt"))
+                {
+                    result = Math.sqrt(Double.parseDouble(right));
+                    System.out.println(result);
+                    clear();
+                    left = "" + result;
+                    refresh();
+                    onRightSide = false;
+                }
+                
+                else if (operator.equals("sin"))
+                {
+                    result = Math.sin(Math.toRadians(Double.parseDouble(right)));
+                    System.out.println(result);
+                    clear();
+                    left = "" + result;
+                    refresh();
+                    onRightSide = false;
+                }
+                
+                else if (operator.equals("cos"))
+                {
+                    result = Math.cos(Math.toRadians(Double.parseDouble(right)));
+                    System.out.println(result);
+                    clear();
+                    left = "" + result;
+                    refresh();
+                    onRightSide = false;
+                }
+
+            }
+                
+        }
+                else
+                    {
 		if(operator.equals("+")){
 			result = Double.parseDouble(left) + Double.parseDouble(right);
 			clear();
@@ -127,7 +285,48 @@ class Calculator {
 			} else 
 				display.append("Error: Divide by zero");
 		}
-		}catch(NumberFormatException nfe){
+        else if (operator.equals("^"))
+        {
+            if (Double.parseDouble(right) == 0)
+            {
+                result = 1.0;
+                clear();
+                left += "" + result;
+                refresh();
+                onRightSide = false;
+            }
+            else if (Double.parseDouble(right) < 0)
+            {
+                double newLeft = 1 / Double.parseDouble(left);
+                result = newLeft;
+                for (int i = 0; i < ((-1) *Double.parseDouble(right)) - 1; i++)
+                {
+                    result = result * result;
+                }
+                clear();
+                left += "" + result;
+                refresh();
+                onRightSide = false;
+
+            }
+            else
+            {
+                result = Double.parseDouble(left);
+                for (int i = 0; i < Double.parseDouble(right) - 1; i++)
+                {
+                    result *= result;
+                }
+                clear();
+                left += "" + result;
+                refresh();
+                onRightSide = false;
+            }
+        }
+        
+        }
+            }
+        }
+            catch(NumberFormatException nfe){
 			display.append("Error: Number Formatting");
 		}
 	}
